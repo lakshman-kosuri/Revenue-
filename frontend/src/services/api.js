07068c5ac401c2cd011088ef0b33c08a282cb689
lakshman-kosuri <kosurilakshman88@gmail.com>
2025-09-28 10:20:54 +0530

    Your commit message here

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://revenue-xz4f.onrender.com/api"; // for production
+const API_URL = "https://revenue-19uf.onrender.com//api"; // for production
 
 
 // ================= AUTH =================
